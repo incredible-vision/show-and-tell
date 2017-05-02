6b73f6802606e2d4b95f02c77382693b01982533
David <swpark6230@gmail.com>
2017-05-02 20:35:44 +0900

    basic show attend and tell model

--- lstm_test.py
+++ lstm_test.py
@@ -28,25 +28,30 @@ class ShowAttendTellModel(nn.Module):
         self.embed = nn.Embedding(vocab_size, embed_size)
         self.lstmcell = nn.LSTMCell(context_size, hidden_size)
 
+        """ define output MLP for word selecting"""
+        self.mlp_w = nn.Parameter(torch.FloatTensor(hidden_size, 1))
+        self.linear = nn.Linear(context_size, vocab_size)
 
 
-    def forward(self, images):
+
+    def forward(self, images, captions):
         """ put input data through cnn """
         features = self.encoder(images) # [batch, 512, 14, 14]
         features = features.view(features.size(0), features.size(1), -1).transpose(2, 1) # [batch, 196, 512]
         context_encode = torch.bmm(features, self.image_att_w.unsqueeze(0).expand(features.size(0), self.image_att_w.size(0), self.image_att_w.size(1))) # [batch, 196, 512]
-
-        h, c = self.init_lstm(features)
+        hidden, c = self.init_lstm(features)
         alpha_list = []
         for t in range(self.opt.time_step):
-
+            captions_cur = captions[:, t, :]
+            embedding = self.embed(captions_cur)
             context, alpha = self.attention_layer(features, context_encode, h)
+            embedding = torch.cat(embedding, context, 1)
             alpha_list.append(alpha)
-            h, c = self.lstmcell()
-
-
+            h, c = self.lstmcell(embedding, hidden)
+            context, beta = self.out_select(context, h)
+            outputs = self.linear(context)
         print(len(features))
-        return features
+        return features, outputs
 
     def init_lstm(self, features):
         features_mean = features.mean(1).squeeze(1)
@@ -61,14 +66,11 @@ class ShowAttendTellModel(nn.Module):
         context = (features * alpha.unsqueeze(2).expand_as(features)).mean(1).squeeze(1)
         return context, alpha
 
+    def out_select(self, context, h):
+        beta = nn.Sigmoid((self.mlp_w).dot(h))
+        context = torch.mul(context,beta)
+        return context, beta
+
     def finetune(self, allow=False):
         for param in self.encoder.parameters():
             param.requires_grad = True if allow else False
-
-
-
-
-
-
-
-
